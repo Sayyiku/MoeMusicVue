e42d1acee92c12a6cfa0ec8e9742a9f2c0a7810a
“Sayyiku” <sayyikue@gmail.com>
2022-03-17 00:27:46 +0800

    add quasar config and hello api request

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/zh-CN'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    lang: quasarLang,
+})
+myApp.mount('#app')
