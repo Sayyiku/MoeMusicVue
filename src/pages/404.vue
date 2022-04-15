d26c4735d677108b76409da1b1c7fb58f938d34e
“sayyiku” <liricarain@gmail.com>
2022-04-15 16:14:41 +0800

    :pencil2: (fix): fix
    fix

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,3 +1,15 @@
+<!--
+ * @Author: Sayyiku
+ * @Date: 2022-04-13 23:55:17
+ * @LastEditors: Sayyiku
+ * @LastEditTime: 2022-04-14 23:19:24
+ * @FilePath: \MoeMusicVue\src\pages\404.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by Sayyiku, All Rights Reserved. 
+-->
+
+
 <template>
   <div>404</div>
 </template>
